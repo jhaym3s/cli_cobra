2591bf62966461e332f2ab86cc0b3c35dbded090
Jhaym3s <jhaymescode@gmail.com>
2023-09-06 18:54:55 +0100

    max

--- main.go
+++ main.go
@@ -8,11 +8,12 @@ func main() {
 
 	number := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 	reversedNumber := reverseSlice(number)
+	maxNumber := findMaxNumber(number)
 	for i := 0; i < len(number)/2; i++ {
 		number[i], number[len(number)-i-1] = number[len(number)-i-1], number[i]
 	}
 
-	fmt.Println(reversedNumber, number)
+	fmt.Println(reversedNumber, number, maxNumber)
 }
 
 func reverseSlice(slice []int) []int {
@@ -23,4 +24,14 @@ func reverseSlice(slice []int) []int {
 		reversed[length-i-1] = v
 	}
 	return reversed
+}
+ 
+func findMaxNumber(slice []int)int{
+	max := slice[0]
+	for _, v := range slice {
+	 if v > max {
+		max = v
+	}
+	}
+	return max
 }
