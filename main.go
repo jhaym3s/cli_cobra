bda3aa116df619addf89feb9bf2427d66a3aa29a
Jhaym3s <jhaymescode@gmail.com>
2023-10-02 15:11:03 +0100

    regexp

--- main.go
+++ main.go
@@ -3,11 +3,16 @@ package main
 import (
 	"flag"
 	"fmt"
+	"regexp"
 )
 
-
-func main()  {
-	dice := flag.String("d","d6","The type of dice to roll. Format is DX where X is an int. Default is D6")
-	flag.Parse()
-	fmt.Println(*dice)
+func main() {
+	dice := flag.String("d", "d4", "The type of dice to roll. Format is DX where X is an int. Default is D6")
+	flag.Parse() //After defining all flags call the function "flag.Parse()" to parse the command line into the defined flags.
+	matched, _ := regexp.Match("d\\d+$", []byte(*dice))
+	if matched {
+		fmt.Printf("You rolled %s\n", *dice)
+	}else{
+		fmt.Printf("Something is wrong with  %s value \n", *dice)
+	}
 }
