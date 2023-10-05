8902439efff2dc464f25da116ac54c6d19a8d0d0
Jhaym3s <jhaymescode@gmail.com>
2023-10-05 13:46:12 +0100

    part one

--- main.go
+++ main.go
@@ -1,74 +1,62 @@
 package main
 
 import (
+	"encoding/csv"
 	"flag"
 	"fmt"
-	"math/rand"
-	"regexp"
-	"sort"
-	"strconv"
-	"time"
+	"os"
+	"strings"
 )
 
+type problems struct {
+	q string
+	a string
+}
+
 func main() {
-	rand.Seed(time.Now().UTC().UnixNano())
-
-	dice := flag.String("d", "d4", "The type of dice to roll. Format is DX where X is an int. Default is D6")
-	numRoll := flag.Int("n", 1, "The number of die rolls. Default is 1")
-	sum := flag.Bool("s",false,"This gives you the sum of all the  rolled dice")
-	adv := flag.Bool("adv", false,"This gives you the highest number of all the  rolled dice")
-	disAd:= flag.Bool("disAd", false,"This gives you the lowest number of all the  rolled dice")
-	flag.Parse() //After defining all flags call the function "flag.Parse()" to parse the command line into the defined flags.
-	matched, _ := regexp.Match("d\\d+$", []byte(*dice))
-	if matched {
-		value := rollDice(dice, numRoll)
-		if *sum {
-			fmt.Printf("Sum of dice is %d\n ", sumDice(value))
-		} 
-		if *adv{
-			advantage(value)
-		}
-		if *disAd{
-			disAdvantage(value)
-		}
-		printDice(value)
+	quizFileName := flag.String("quiz", "quiz.cvs", "")
+	flag.Parse()
+	cvsFile, err := os.Open(*quizFileName)
+	if err != nil {
+		exit(fmt.Sprintf("Failed to open your CVS file %s", *quizFileName))
 	} else {
-		fmt.Printf("Something is wrong dice format is dx  %s \n", *dice)
+		cvsReader := csv.NewReader(cvsFile)
+		cont, err := cvsReader.ReadAll()
+		if err != nil {
+			exit("Something is wrong with your cvs file")
+		} else {
+			problems := parseLines(cont)
+			correctAnswers := 0
+			for i, p := range problems {
+				fmt.Printf("Question %d %s = \n", i+1, p.q)
+				var answer string
+				fmt.Scanf("%s\n", &answer)
+				if answer == p.a{
+					correctAnswers++ 
+					fmt.Println("You are correct")
+				}else{
+					fmt.Println("Mumu")
+				}
+				
+			}
+			fmt.Printf("You had %d correct out of %d\n",correctAnswers,len(problems))
+		}
+
 	}
 }
 
-func rollDice(dice *string, numRoll *int) []int {
-	var rolls []int
-	stringDiceSize := (*dice)[1:]
-	diceSize, _ := strconv.Atoi(stringDiceSize)
-	for i := 0; i < *numRoll; i++ {
-		roll := rand.Intn(diceSize) + 1
-		fmt.Printf("You chose %d \n", roll)
-		rolls = append(rolls, roll)
-	}
-	return rolls
-}
-
-func printDice(roll []int) {
-	for i, v := range roll {
-		fmt.Printf("Roll %d was %d\n", i+1, v)
+func parseLines(lines [][]string) []problems {
+	ret := make([]problems, len(lines))
+	for i, v := range lines {
+		ret[i] = problems{
+			q: v[0],
+			a: strings.TrimSpace(v[1]),
+		}
 	}
 
+	return ret
 }
-func sumDice(dice []int ) int {
-	sum := 0
-	for _, v := range dice {
-		sum += v
-	}
-	return sum
-}
-
-func advantage(dice []int)  {
-	sort.Ints(dice)
-	fmt.Printf("advantage is %d", dice[len(dice)-1])
-}
-func disAdvantage(dice []int)  {
-	sort.Ints(dice)
-	fmt.Printf("advantage is %d", dice[0])
-	//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
+func exit(errMsg string) {
+	fmt.Println(errMsg)
+	os.Exit(1)
 }
